ae0978320e80a77abe5d93d20701c9323b55c6dd
DESK <criscianotsi@gmail.com>
2019-07-09 23:44:52 -0300

    Alter documentation

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ cap = cv2.VideoCapture(0)
 # loop
 while True:
     ret, frame = cap.read()
+    # imshow open window name Video
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+#After finish, release video
 cap.release()
 cv2.destroyAllWindows()
